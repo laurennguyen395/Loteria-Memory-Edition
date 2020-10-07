da8466b41f8f5e9fc32154c34f58ed1b87ea3309
laurennguyen395 <laurennguyen395@gmail.com>
2020-10-06 19:42:07 -0700

    Getting ready for actualy game play.

--- js/app.js
+++ js/app.js
@@ -31,8 +31,31 @@ let setGamePlay = {
         const timerInterval = setInterval(() => {
             //What we do each second
             if (timer === 0) {
-                clearInterval(timerInterval)
+                
                 console.log('Find all of the card values')
+
+                const removeCont = document.querySelector('.container')
+                removeCont.parentNode.removeChild(removeCont)
+
+                const removeT = document.querySelector('.fifteenSeconds')
+                removeT.parentNode.removeChild(removeT)
+
+                const computerChooseP = document.createElement('p')
+                computerChooseP.setAttribute('class' , 'computerChooseP')
+                document.body.appendChild(computerChooseP)
+                document.querySelector('.computerChooseP').innerHTML = "Can you find all the 'computerChooser()'"
+
+
+
+                const readyBtnContainer = document.querySelector('div')
+                readyBtnContainer.setAttribute('class' , 'readyBtnContainer')
+                document.body.appendChild(readyBtnContainer)
+                
+                const readyButton = document.createElement('button')
+                readyButton.setAttribute('class' , 'readyButton')
+                readyBtnContainer.appendChild(readyButton)
+                document.querySelector('.readyButton').innerHTML = 'Ready'
+                clearInterval(timerInterval)
 
                 //*** NEED TO *** Flip cards, set new 30s timer, 
                 //add click events on all cards. 
@@ -53,119 +76,20 @@ let setGamePlay = {
         container.setAttribute('class', 'container')
         document.body.appendChild(container)
 
-        //const cardGrid = document.querySelector('div')
-        //cardGrid.setAttribute('class' , 'cardGrid')
-        //container.appendChild(cardGrid)
+
         const cardGrid = document.createElement('div')
         cardGrid.setAttribute('class', 'parent')
         container.appendChild(cardGrid)
 
 
         //Setting each Div
-        let div1 = document.createElement('img')
-        div1.setAttribute('class', 'cardContainer')
-        div1.setAttribute('id', 'div1')
-        cardGrid.appendChild(div1)
-
-
-
-
-        const div2 = document.createElement('img')
-        div2.setAttribute('class', 'cardContainer')
-        div2.setAttribute('id', 'div2')
-        cardGrid.appendChild(div2)
-
-
-
-        const div3 = document.createElement('img')
-        div3.setAttribute('class', 'cardContainer')
-        div3.setAttribute('id', 'div3')
-        cardGrid.appendChild(div3)
-
-        const div4 = document.createElement('img')
-        div4.setAttribute('class', 'cardContainer')
-        div4.setAttribute('id', 'div4')
-        cardGrid.appendChild(div4)
-
-        const div5 = document.createElement('img')
-        div5.setAttribute('class', 'cardContainer')
-        div5.setAttribute('id', 'div5')
-        cardGrid.appendChild(div5)
-
-        const div6 = document.createElement('img')
-        div6.setAttribute('class', 'cardContainer')
-        div6.setAttribute('id', 'div6')
-        cardGrid.appendChild(div6)
-
-        const div7 = document.createElement('img')
-        div7.setAttribute('class', 'cardContainer')
-        div7.setAttribute('id', 'div7')
-        cardGrid.appendChild(div7)
-
-
-
-        const div8 = document.createElement('img')
-        div8.setAttribute('id', 'div8')
-        div8.setAttribute('class', 'cardContainer')
-        cardGrid.appendChild(div8)
-
-        const div9 = document.createElement('img')
-        div9.setAttribute('class', 'cardContainer')
-        div9.setAttribute('id', 'div9')
-        cardGrid.appendChild(div9)
-
-        const div10 = document.createElement('img')
-        div10.setAttribute('class', 'cardContainer')
-        div10.setAttribute('id', 'div10')
-        cardGrid.appendChild(div10)
-
-        const div11 = document.createElement('img')
-        div11.setAttribute('class', 'cardContainer')
-        div11.setAttribute('id', 'div11')
-        cardGrid.appendChild(div11)
-
-
-
-
-        const div12 = document.createElement('img')
-        div12.setAttribute('class', 'cardContainer')
-        div12.setAttribute('id', 'div12')
-        cardGrid.appendChild(div12)
-
-        const div13 = document.createElement('img')
-        div13.setAttribute('class', 'cardContainer')
-        div13.setAttribute('id', 'div13')
-        cardGrid.appendChild(div13)
-
-        const div14 = document.createElement('img')
-        div14.setAttribute('class', 'cardContainer')
-        div14.setAttribute('id', 'div14')
-        cardGrid.appendChild(div14)
-
-        const div15 = document.createElement('img')
-        div15.setAttribute('class', 'cardContainer')
-        div15.setAttribute('id', 'div15')
-        cardGrid.appendChild(div15)
-
-
-
-
-        const div16 = document.createElement('img')
-        div16.setAttribute('class', 'cardContainer')
-        div16.setAttribute('id', 'div16')
-        cardGrid.appendChild(div16)
-
-        const div17 = document.createElement('img')
-        div17.setAttribute('class', 'cardContainer')
-        div17.setAttribute('id', 'div17')
-        cardGrid.appendChild(div17)
-
-        const div18 = document.createElement('img')
-        div18.setAttribute('class', 'cardContainer')
-        div18.setAttribute('id', 'div18')
-        cardGrid.appendChild(div18)
-
-
+        for (let i = 1; i < 19; i++) {
+            let div = document.createElement('img')
+            div.setAttribute('class', 'cardContainer')
+            div.setAttribute('id', 'div' + i)
+            cardGrid.appendChild(div)
+        }
+        
 
     },
     rememberCards: [],
@@ -190,6 +114,11 @@ let setGamePlay = {
     deal: function () {
         this.getCards()
         this.shuffle()
+        
+        //for (let i =1; i < 19; i++) {
+
+        //}
+
         let playerCard1 = setGamePlay.rememberCards.shift()
         let playerCard2 = setGamePlay.rememberCards.shift()
         let playerCard3 = setGamePlay.rememberCards.shift()
@@ -230,7 +159,7 @@ let setGamePlay = {
     },
     
 }
-console.log(setGamePlay.getCards())
+//console.log(setGamePlay.getCards())
 
 
 
@@ -242,9 +171,16 @@ console.log(setGamePlay.getCards())
 
 
 document.querySelector('.startButton').addEventListener('click', () => { setGamePlay.clearPage(), setGamePlay.setTable(), setGamePlay.setInterval1(), setGamePlay.deal() })
+//document.querySelector('.readyButton').addEventListener('click' , () =>)
 
-
-
+//setInterval2() ==> When setInterval1 === 0: 
+    // - ??? clearPage2
+    // - create a computer chooses function
+            // --This will shuffle through the array of cards and pick a random index.
+            // --This is the card the player will have to find and what we will have to 
+                //compare to ??? playerChooser() ???
+    // - set a "Ready?" button
+    // - set an event listener to 
 
 
 
@@ -252,12 +188,11 @@ document.querySelector('.startButton').addEventListener('click', () => { setGame
 
 
     /*
-    computerChooses()
+    computerChooser()
     playerWin()
 
 
     playerLose()
-    setInterval() ==> When setInterval1 === 0 reset interval to 30s.
     resetTable()
     flip()
     */
